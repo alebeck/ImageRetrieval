95f06432733350a6870e6bfdcef2073cc088f514
Alexander Becker <alexander.becker@tum.de>
2019-05-17 14:02:38 +0200

    Implement datataset

--- datasets/day_night.py
+++ datasets/day_night.py
@@ -1,13 +1,29 @@
+import os
+
 from torch.utils.data import Dataset
+from torchvision.transforms import ToTensor
+from PIL import Image
 
 
 class DayNightDataset(Dataset):
 
     def __init__(self, path_day, path_night):
-        pass
+
+        self.images_day, self.images_night = [], []
+        to_tensor = ToTensor()
+
+        for path in os.listdir(path_day):
+            with open(path, 'rb') as file:
+                img = Image.open(file)
+                self.images_day.append(to_tensor(img))
+
+        for path in os.listdir(path_night):
+            with open(path, 'rb') as file:
+                img = Image.open(file)
+                self.images_night.append(to_tensor(img))
 
     def __len__(self):
-        pass
+        return min(len(self.images_day), len(self.images_night))
 
     def __getitem__(self, index):
-        pass # TODO return tuple (day_img, night_img)
+        return self.images_day[index], self.images_night[index]
