d60c22ecabe7dc6256b40354333a4c80403f02df
Alexander Becker <alexander.becker@tum.de>
2019-05-17 17:37:19 +0200

    Add resize transform to dataset

--- datasets/day_night.py
+++ datasets/day_night.py
@@ -1,26 +1,29 @@
 import os
 
 from torch.utils.data import Dataset
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, Resize, Compose
 from PIL import Image
 
 
 class DayNightDataset(Dataset):
 
-    def __init__(self, path_day, path_night):
-
+    def __init__(self, path_day, path_night, img_size=128):
         self.images_day, self.images_night = [], []
-        to_tensor = ToTensor()
 
-        for path in os.listdir(path_day):
-            with open(path, 'rb') as file:
-                img = Image.open(file)
-                self.images_day.append(to_tensor(img))
+        transform = Compose([
+            Resize(img_size),
+            ToTensor()
+        ])
 
-        for path in os.listdir(path_night):
-            with open(path, 'rb') as file:
+        for filename in os.listdir(path_day):
+            with open(os.path.join(path_day, filename), 'rb') as file:
                 img = Image.open(file)
-                self.images_night.append(to_tensor(img))
+                self.images_day.append(transform(img))
+
+        for filename in os.listdir(path_night):
+            with open(os.path.join(path_night, filename), 'rb') as file:
+                img = Image.open(file)
+                self.images_night.append(transform(img))
 
     def __len__(self):
         return min(len(self.images_day), len(self.images_night))
