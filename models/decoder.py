f74b7556650f6896915f5c8ef0f84c050efd678c
Arman Mielke <arman.mielke@yahoo.de>
2019-05-20 20:10:23 +0200

    Add rudimentary encoder and decoder

--- models/decoder.py
+++ models/decoder.py
@@ -2,5 +2,21 @@ from torch import nn
 
 
 class Decoder(nn.Module):
-    def forward(self, input):
-        pass
+
+    def __init__(self):
+        super().__init__()
+
+        # input (channels, width, height) is                                         (32, 16,  16)
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1),  # (16, 32,  32)
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),   # (8,  64,  64)
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(8, 3, 3, stride=2, padding=1, output_padding=1),    # (3,  128, 128)
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        self.decoder(x)
--- models/encoder.py
+++ models/encoder.py
@@ -1,11 +1,25 @@
 from torch import nn
 
 
-class Encoder(nn.Module): # TODO implement
+class Encoder(nn.Module):
 
     def __init__(self):
         super().__init__()
-        #self.conv1 = nn.Conv2d(3)
 
-    def forward(self, input):
-        pass
+        # input (channels, width, height) is    (3,  128, 128)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 8, 3, padding=1),    # (8,  128, 128)
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),        # (8,  64,  64)
+
+            nn.Conv2d(8, 16, 3, padding=1),   # (16, 64,  64)
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),        # (16, 32,  32)
+
+            nn.Conv2d(16, 32, 3, padding=1),  # (32, 32,  32)
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),        # (32, 16,  16)
+        )
+
+    def forward(self, x):
+        return self.encoder(x)
