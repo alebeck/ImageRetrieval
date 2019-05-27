2eae187d556be7ffcb0819d56900e4abe9c9729f
Arman Mielke <arman.mielke@yahoo.de>
2019-05-28 00:25:11 +0200

    Increase encoder size

--- models/decoder.py
+++ models/decoder.py
@@ -6,12 +6,12 @@ class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        # input (channels, width, height) is                                         (32, 16,  16)
+        # input (channels, width, height) is                                         (64, 16,  16)
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1),  # (16, 32,  32)
+            nn.ConvTranspose2d(64, 24, 3, stride=2, padding=1, output_padding=1),  # (24, 32,  32)
             nn.ReLU(),
 
-            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),   # (8,  64,  64)
+            nn.ConvTranspose2d(24, 8, 3, stride=2, padding=1, output_padding=1),   # (8, 64,  64)
             nn.ReLU(),
 
             nn.ConvTranspose2d(8, 3, 3, stride=2, padding=1, output_padding=1),    # (3,  128, 128)
--- models/encoder.py
+++ models/encoder.py
@@ -8,17 +8,25 @@ class Encoder(nn.Module):
 
         # input (channels, width, height) is    (3,  128, 128)
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 8, 3, padding=1),    # (8,  128, 128)
+            nn.Conv2d(3, 6, 3, padding=1),    # (6,  128, 128)
             nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),        # (8,  64,  64)
+            nn.Conv2d(6, 10, 3, padding=1),   # (10, 128, 128)
+            nn.ReLU(),
+            nn.Conv2d(10, 16, 3, padding=1),  # (16, 128, 128)
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),        # (16, 64,  64)
 
-            nn.Conv2d(8, 16, 3, padding=1),   # (16, 64,  64)
+            nn.Conv2d(16, 24, 3, padding=1),  # (24, 64,  64)
             nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),        # (16, 32,  32)
+            nn.Conv2d(24, 32, 3, padding=1),  # (32, 64,  64)
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),        # (32, 32,  32)
 
-            nn.Conv2d(16, 32, 3, padding=1),  # (32, 32,  32)
+            nn.Conv2d(32, 48, 3, padding=1),  # (48, 32,  32)
             nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),        # (32, 16,  16)
+            nn.Conv2d(48, 64, 3, padding=1),  # (64, 32,  32)
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),        # (64, 16,  16)
         )
 
     def forward(self, x):
