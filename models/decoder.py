4cf7d167a2ff792dd0a29342a8ac14defae40900
Alexander Becker <alexander.becker@tum.de>
2019-05-28 11:54:52 +0200

    Enlarge/Refactor encoder and decoder

--- models/decoder.py
+++ models/decoder.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch.nn.functional as f
 
 
 class Decoder(nn.Module):
@@ -6,17 +7,27 @@ class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        # input (channels, width, height) is                                         (64, 16,  16)
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 24, 3, stride=2, padding=1, output_padding=1),  # (24, 32,  32)
-            nn.ReLU(),
+        self.convt1_1 = nn.ConvTranspose2d(512, 256, 3, stride=1, padding=1, output_padding=0)
+        self.convt1_2 = nn.ConvTranspose2d(256, 128, 3, stride=1, padding=1, output_padding=0)
+        self.convt1_3 = nn.ConvTranspose2d(128, 128, 3, stride=2, padding=1, output_padding=1)
 
-            nn.ConvTranspose2d(24, 8, 3, stride=2, padding=1, output_padding=1),   # (8, 64,  64)
-            nn.ReLU(),
+        self.convt2_1 = nn.ConvTranspose2d(128, 64, 3, stride=1, padding=1, output_padding=0)
+        self.convt2_2 = nn.ConvTranspose2d(64, 64, 3, stride=1, padding=1, output_padding=0)
+        self.convt2_3 = nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1)
 
-            nn.ConvTranspose2d(8, 3, 3, stride=2, padding=1, output_padding=1),    # (3,  128, 128)
-            nn.ReLU(),
-        )
+        self.convt3_1 = nn.ConvTranspose2d(32, 16, 3, stride=1, padding=1, output_padding=0)
+        self.convt3_2 = nn.ConvTranspose2d(16, 3, 3, stride=2, padding=1, output_padding=1)
 
     def forward(self, x):
-        return self.decoder(x)
+        x = f.relu(self.convt1_1(x))
+        x = f.relu(self.convt1_2(x))
+        x = f.relu(self.convt1_3(x))
+
+        x = f.relu(self.convt2_1(x))
+        x = f.relu(self.convt2_2(x))
+        x = f.relu(self.convt2_3(x))
+
+        x = f.relu(self.convt3_1(x))
+        x = f.relu(self.convt3_2(x))
+
+        return x
--- models/encoder.py
+++ models/encoder.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch.nn.functional as f
 
 
 class Encoder(nn.Module):
@@ -6,28 +7,33 @@ class Encoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        # input (channels, width, height) is    (3,  128, 128)
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, 6, 3, padding=1),    # (6,  128, 128)
-            nn.ReLU(),
-            nn.Conv2d(6, 10, 3, padding=1),   # (10, 128, 128)
-            nn.ReLU(),
-            nn.Conv2d(10, 16, 3, padding=1),  # (16, 128, 128)
-            nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),        # (16, 64,  64)
+        self.conv1_1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv1_2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.pool1 = nn.MaxPool2d(2, stride=2)
 
-            nn.Conv2d(16, 24, 3, padding=1),  # (24, 64,  64)
-            nn.ReLU(),
-            nn.Conv2d(24, 32, 3, padding=1),  # (32, 64,  64)
-            nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),        # (32, 32,  32)
+        self.conv2_1 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv2_2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv2_3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.pool2 = nn.MaxPool2d(2, stride=2)
 
-            nn.Conv2d(32, 48, 3, padding=1),  # (48, 32,  32)
-            nn.ReLU(),
-            nn.Conv2d(48, 64, 3, padding=1),  # (64, 32,  32)
-            nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),        # (64, 16,  16)
-        )
+        self.conv3_1 = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv3_2 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv3_3 = nn.Conv2d(256, 512, 3, padding=1)
+        self.pool3 = nn.MaxPool2d(2, stride=2)
 
     def forward(self, x):
-        return self.encoder(x)
+        x = f.relu(self.conv1_1(x))
+        x = f.relu(self.conv1_2(x))
+        x = self.pool1(x)
+
+        x = f.relu(self.conv2_1(x))
+        x = f.relu(self.conv2_2(x))
+        x = f.relu(self.conv2_3(x))
+        x = self.pool2(x)
+
+        x = f.relu(self.conv3_1(x))
+        x = f.relu(self.conv3_2(x))
+        x = f.relu(self.conv3_3(x))
+        x = self.pool3(x)
+
+        return x
