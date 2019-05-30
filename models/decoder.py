2da990074a5d9912598c5921f415e82958ea7791
Alexander Becker <alexander.becker@tum.de>
2019-05-30 18:40:28 +0200

    Change model architecture

--- models/decoder.py
+++ models/decoder.py
@@ -7,16 +7,17 @@ class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.convt1_1 = nn.ConvTranspose2d(512, 256, 3, stride=1, padding=1, output_padding=0)
-        self.convt1_2 = nn.ConvTranspose2d(256, 128, 3, stride=1, padding=1, output_padding=0)
-        self.convt1_3 = nn.ConvTranspose2d(128, 128, 3, stride=2, padding=1, output_padding=1)
+        self.convt1_1 = nn.ConvTranspose2d(512, 512, 3, stride=1, padding=1, output_padding=0)
+        self.convt1_2 = nn.ConvTranspose2d(512, 256, 3, stride=1, padding=1, output_padding=0)
+        self.convt1_3 = nn.ConvTranspose2d(256, 256, 3, stride=2, padding=1, output_padding=1)
 
-        self.convt2_1 = nn.ConvTranspose2d(128, 64, 3, stride=1, padding=1, output_padding=0)
-        self.convt2_2 = nn.ConvTranspose2d(64, 64, 3, stride=1, padding=1, output_padding=0)
-        self.convt2_3 = nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1)
+        self.convt2_1 = nn.ConvTranspose2d(256, 128, 3, stride=1, padding=1, output_padding=0)
+        self.convt2_2 = nn.ConvTranspose2d(128, 128, 3, stride=1, padding=1, output_padding=0)
+        self.convt2_3 = nn.ConvTranspose2d(128, 64, 3, stride=1, padding=1, output_padding=0)
+        self.convt2_4 = nn.ConvTranspose2d(64, 64, 3, stride=2, padding=1, output_padding=1)
 
-        self.convt3_1 = nn.ConvTranspose2d(32, 16, 3, stride=1, padding=1, output_padding=0)
-        self.convt3_2 = nn.ConvTranspose2d(16, 3, 3, stride=2, padding=1, output_padding=1)
+        self.convt3_1 = nn.ConvTranspose2d(64, 32, 3, stride=1, padding=1, output_padding=0)
+        self.convt3_2 = nn.ConvTranspose2d(32, 3, 3, stride=2, padding=1, output_padding=1)
 
     def forward(self, x):
         x = f.relu(self.convt1_1(x))
@@ -26,6 +27,7 @@ class Decoder(nn.Module):
         x = f.relu(self.convt2_1(x))
         x = f.relu(self.convt2_2(x))
         x = f.relu(self.convt2_3(x))
+        x = f.relu(self.convt2_4(x))
 
         x = f.relu(self.convt3_1(x))
         x = f.relu(self.convt3_2(x))
--- models/encoder.py
+++ models/encoder.py
@@ -10,14 +10,18 @@ class LowerEncoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv1_1 = nn.Conv2d(3, 16, 3, padding=1)
-        self.conv1_2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv1_1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv1_2 = nn.Conv2d(32, 64, 3, padding=1)
         self.pool1 = nn.MaxPool2d(2, stride=2)
+
+        self.conv2_1 = nn.Conv2d(64, 64, 3, padding=1)
 
     def forward(self, x):
         x = f.relu(self.conv1_1(x))
         x = f.relu(self.conv1_2(x))
         x = self.pool1(x)
+
+        x = f.relu(self.conv2_1(x))
 
         return x
 
@@ -30,20 +34,20 @@ class UpperEncoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv2_1 = nn.Conv2d(32, 64, 3, padding=1)
-        self.conv2_2 = nn.Conv2d(64, 64, 3, padding=1)
-        self.conv2_3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv2_2 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv2_3 = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv2_4 = nn.Conv2d(128, 256, 3, padding=1)
         self.pool2 = nn.MaxPool2d(2, stride=2)
 
-        self.conv3_1 = nn.Conv2d(128, 128, 3, padding=1)
-        self.conv3_2 = nn.Conv2d(128, 256, 3, padding=1)
-        self.conv3_3 = nn.Conv2d(256, 512, 3, padding=1)
+        self.conv3_1 = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_2 = nn.Conv2d(256, 512, 3, padding=1)
+        self.conv3_3 = nn.Conv2d(512, 512, 3, padding=1)
         self.pool3 = nn.MaxPool2d(2, stride=2)
 
     def forward(self, x):
-        x = f.relu(self.conv2_1(x))
         x = f.relu(self.conv2_2(x))
         x = f.relu(self.conv2_3(x))
+        x = f.relu(self.conv2_4(x))
         x = self.pool2(x)
 
         x = f.relu(self.conv3_1(x))
