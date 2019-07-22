b1becbc1dcf6ed0664cd1b57b284f4a5ab2c5b92
Arman Mielke <arman.mielke@yahoo.de>
2019-07-22 13:57:09 +0200

    Train baseline model with reconstruction loss

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ from utils.trainer import Trainer
 config = TrainingConfig(
     dataset=DayNightDataset,
     dataset_args={
-        'paths_day': [],
-        'paths_night': [],
+        'paths_day': [
+            'data/synthia_seq4_repacked/day/left',
+            'data/synthia_seq4_repacked/day/right',
+        ],
+        'paths_night': [
+            'data/synthia_seq4_repacked/night/left',
+            'data/synthia_seq4_repacked/night/right',
+        ],
         'transform': Compose([
             CenterCrop(760),
             Resize(128),
@@ -20,11 +26,11 @@ config = TrainingConfig(
     model=SimpleModel,
     checkpoint_path=None,
     model_args={},
-    batch_size=1,
-    epochs=10,
+    batch_size=32,
+    epochs=71,
     val_size=0.2,
-    log_path='log',
-    save_every=100
+    log_path='drive/My Drive/adl4cv/training-logs/baseline-reconstruction-loss',
+    save_every=5
 )
 
 trainer = Trainer(config)
